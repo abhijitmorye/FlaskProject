2f9b41b90c119df00b9f32500f6e7fa1b17a7895
abhijitmorye <abhijitmorye>
2021-06-05 16:17:59 +0530

    Flask REST API - Weather App Done - 05062021

--- WeatherApp/app.py
+++ WeatherApp/app.py
@@ -29,7 +29,7 @@ class HistoricData(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html', ResultExists=False)
+    return render_template('index.html', ResultExists=False, searchForHistoric=False)
 
 
 @app.route('/search', methods=['POST'])
@@ -47,18 +47,27 @@ def serach():
     max_temparature = result['main']['temp_max']
     humidity = result['main']['humidity']
 
-    cityExists = HistoricData.query.filter_by(cityname=cityname).first()
+    cityExists = HistoricData.query.filter_by(cityname=cityname).all()
     todayDate = str(datetime.datetime.now()).split()
-
-    if cityExists is not None:
-        if cityExists.date == todayDate:
+    print(todayDate)
+    flag = False
+    cityCurrent = None
+    if len(cityExists) > 0:
+        for city in cityExists:
+            cityCurrent = city
+            if cityCurrent.date == todayDate[0]:
+                flag = True
+        if flag:
+            print("Same date and same country --{}".format(cityCurrent.cityname))
             pass
         else:
+            print("New date and same country --{}".format(cityname))
             newTemp = HistoricData(cityname=cityname, temparature=temparature,
                                    min_temparature=min_temparature, max_temparature=max_temparature, humidity=humidity)
-        db.session.add(newTemp)
-        db.session.commit()
+            db.session.add(newTemp)
+            db.session.commit()
     else:
+        print("New country --{}".format(cityname))
         newTemp = HistoricData(cityname=cityname, temparature=temparature,
                                min_temparature=min_temparature, max_temparature=max_temparature, humidity=humidity)
         db.session.add(newTemp)
@@ -73,7 +82,29 @@ def serach():
         'temp_max': result['main']['temp_max'],
         'humidity': result['main']['humidity']
     }
-    return render_template('index.html', ResultExists=True, context=context)
+    return render_template('index.html', ResultExists=True, searchForHistoric=False, context=context)
+
+
+@app.route('/viewpastweather/<city_name>')
+def viewPastWeather(city_name):
+    print(city_name.title())
+    cityExists = HistoricData.query.filter_by(
+        cityname=city_name.title()).all()
+    if cityExists is not None:
+        cityDates = []
+        cityTemps = []
+        for city in cityExists:
+            cityDates.append(int(''.join(city.date.split('-'))))
+            cityTemps.append(float(city.temparature))
+            print(''.join(city.date.split('-')))
+            print(city.temparature)
+        context = {
+            'cityDate': cityDates,
+            'cityTemps': cityTemps
+        }
+        return render_template('index.html', searchForHistoric=True, context=context)
+    else:
+        return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
